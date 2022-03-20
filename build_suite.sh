998eb89b4e0482ebd9f128f82fdc3ff7951279ac
Phillip A Lane <pal0009@euler.ece.uah.edu>
2022-03-19 19:18:23 -0500

    config file complete

--- build_suite.sh
+++ build_suite.sh
@@ -5,12 +5,12 @@ source /opt/intel/oneapi/setvars.sh
 
 export CFFILE=icx-2021.3.0-linux.cfg
 
-#cp ./$CFFILE /apps/arch/cpu2017/config
+cp ./$CFFILE /apps/arch/cpu2017/config
 
 export ROOTDIR=/apps/arch/cpu2017
 export CONFIG=$ROOTDIR/config/$CFFILE
 export RUNCPU=$ROOTDIR/bin/runcpu
-export BUILD="all"
-#export BUILD="602.gcc_s"
+#export BUILD="all"
+export BUILD="503.bwaves_r"
 
 $RUNCPU --size=test --config=$CONFIG --action build $BUILD
