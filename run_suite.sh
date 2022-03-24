d41b3b3776512f264785581a793231fec7571037
Phillip A Lane <pal0009@euler.ece.uah.edu>
2022-03-24 16:54:58 -0500

    starting runs

--- run_suite.sh
+++ run_suite.sh
@@ -10,7 +10,8 @@ cp ./$CFFILE /apps/arch/cpu2017/config
 export ROOTDIR=/apps/arch/cpu2017
 export CONFIG=$ROOTDIR/config/$CFFILE
 export RUNCPU=$ROOTDIR/bin/runcpu
-#export BUILD="all"
-export BUILD="603.bwaves_s"
+export BUILD="all"
+#export BUILD="603.bwaves_s"
 
-$RUNCPU --size=test --config=$CONFIG $BUILD
+$RUNCPU --size=ref --config=$CONFIG $BUILD
+$RUNCPU --size=ref --reportable --config=$CONFIG $BUILD
