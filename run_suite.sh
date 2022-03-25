31c9bbb99f2163716bdaa887ddd000e82e376884
Phillip A Lane <pal0009@euler.ece.uah.edu>
2022-03-25 12:20:40 -0500

    fixing issues

--- run_suite.sh
+++ run_suite.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 ulimit -s unlimited
-source /opt/intel/oneapi/setvars.sh
 
 export CFFILE=aocc-3.1.0-linux.cfg
+export REFFILE=aocc-reportable-3.1.0-linux.cfg
 
 cp ./$CFFILE /apps/arch/cpu2017/config
+cp ./$REFFILE /apps/arch/cpu2017/config
 
 export ROOTDIR=/apps/arch/cpu2017
 export CONFIG=$ROOTDIR/config/$CFFILE
+export REFCFG=$ROOTDIR/config/$REFFILE
 export RUNCPU=$ROOTDIR/bin/runcpu
 export BUILD="all"
 #export BUILD="603.bwaves_s"
 
 $RUNCPU --size=ref --config=$CONFIG $BUILD
-$RUNCPU --size=ref --reportable --config=$CONFIG $BUILD
+$RUNCPU --size=ref --reportable --config=$REFCFG $BUILD
