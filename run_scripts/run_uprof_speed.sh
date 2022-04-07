a55f0b9f83f009b5e6122c4e0e4bd8991b6c5677
Phillip A Lane <pal0009@euler.ece.uah.edu>
2022-04-06 23:43:54 -0500

    preparing

--- run_scripts/run_uprof_speed.sh
+++ run_scripts/run_uprof_speed.sh
@@ -3,6 +3,9 @@
 ulimit -s unlimited
 export OMP_STACKSIZE=192M
 export OMP_NUM_THREADS=48
+#export OMP_SCHEDULE=static
+source /opt/setenv_AOCC.sh
+source /opt/intel/oneapi/setvars.sh
 
 cd /home/student/pal0009/CPE-631-Term-Project/benchspec/CPU
 
@@ -10,73 +13,73 @@ function flags {
     local benchname=$1
 
     case $benchname in 
-        perlbench_r)
+        perlbench_s)
             echo "-I./lib checkspam.pl 2500 5 25 11 150 1 1 1 1"
         ;;
-        gcc_r)
+        gcc_s)
             echo "gcc-pp.c -O3 -finline-limit=0 -fif-conversion -fif-conversion2 -o gcc-pp.opts-O3_-finline-limit_0_-fif-conversion_-fif-conversion2.s"
         ;;
-        bwaves_r)
+        bwaves_s)
             echo "bwaves_1 < bwaves_1.in"
         ;;
-        mcf_r)
+        mcf_s)
             echo "inp.in"
         ;;
-        cactuBSSN_r)
+        cactuBSSN_s)
             echo "spec_ref.par"
         ;;
-        namd_r)
+        namd_s)
             echo "--input apoa1.input --output apoa1.ref.output --iterations 65"
         ;;
-        parest_r)
+        parest_s)
             echo "ref.prm"
         ;;
-        povray_r)
+        povray_s)
             echo "SPEC-benchmark-ref.ini"
         ;;
-        lbm_r)
+        lbm_s)
             echo "3000 reference.dat 0 0 100_100_130_ldc.of"
         ;;
-        omnetpp_r)
+        omnetpp_s)
             echo "-c General -r 0"
         ;;
-        wrf_r)
+        wrf_s)
             echo ""
         ;;
-        xalancbmk_r)
+        xalancbmk_s)
             echo "-v t5.xml xalanc.xsl"
         ;;
-        x264_r)
+        x264_s)
             echo "--pass 1 --stats x264_stats.log --bitrate 1000 --frames 1000 -o BuckBunny_New.264 BuckBunny.yuv 1280x720"
         ;;
-        blender_r)
+        blender_s)
             echo "sh3_no_char.blend --render-output sh3_no_char_ --threads 1 -b -F RAWTGA -s 849 -e 849 -a"
         ;;
-        cam4_r)
+        cam4_s)
             echo ""
         ;;
-        deepsjeng_r)
+        deepsjeng_s)
             echo "ref.txt"
         ;;
-        imagick_r)
+        imagick_s)
             echo "-limit disk 0 refrate_input.tga -edge 41 -resample 181% -emboss 31 -colorspace YUV -mean-shift 19x19+15% -resize 30% refrate_output.tga"
         ;;
-        leela_r)
+        leela_s)
             echo "ref.sgf"
         ;;
-        nab_r)
+        nab_s)
            echo "1am0 1122214447 122" 
         ;;
-        exchange2_r)
+        exchange2_s)
             echo "6"
         ;;
-        fotonik3d_r)
+        fotonik3d_s)
             echo ""
         ;;
-        roms_r)
+        roms_s)
             echo "< ocean_benchmark2.in.x"
         ;;
-        xz_r)
+        xz_s)
             echo "cld.tar.xz 160 19cf30ae51eddcbefda78dd06014b4b96281456e078ca7c13e1c0c9e6aaea8dff3efb4ad6b0456697718cede6bd5454852652806a657bb56e07d61128434b474 59796407 61004416 6"
         ;;
     esac
@@ -89,41 +92,35 @@ function run_benchmark {
     local rundir=$4
 
     echo "instance $instance"
-    local benchname=$(cut -d '.' -f2- <<< $benchmark)
-
-    #printf './%s_base.aocc-3.3.1.0-m64 %s > %s/%s.out 2>> %s/%s.err\n' \
-    #    $benchname \
-    #    "$(flags $benchname)" \
-    #    $outloc \
-    #    $instance \
-    #    $outloc \
-    #    $instance   
+    local benchname=$(cut -d '.' -f2- <<< $benchmark) 
 
     let core=$instance-1
 
     export CORE_PIN="taskset -c $core"
 
     if [ $core = 0 ]; then
-        export UPROF="rm -rf /home/student/pal0009/CPE-631-Term-Project/uprof_results/$benchmark ; mkdir /home/student/pal0009/CPE-631-Term-Project/uprof_results/$benchmark ; AMDuProfPcm -r -i /home/student/pal0009/CPE-631-Term-Project/spec-config.conf -a -C -D /home/student/pal0009/CPE-631-Term-Project/uprof_results/$benchmark/events.csv -o /home/student/pal0009/CPE-631-Term-Project/uprof_results/$benchmark/metrics.csv -- "
+        rm -rf /home/student/pal0009/CPE-631-Term-Project/uprof_results/$benchmark
+        mkdir /home/student/pal0009/CPE-631-Term-Project/uprof_results/$benchmark
+        export UPROF="AMDuProfPcm -r -i /home/student/pal0009/CPE-631-Term-Project/spec-config.conf -a -C -D /home/student/pal0009/CPE-631-Term-Project/uprof_results/$benchmark/events.csv -o /home/student/pal0009/CPE-631-Term-Project/uprof_results/$benchmark/metrics.csv -- "
     else
         export UPROF=""
     fi
 
     #benchmarks with different names for the executable than the folder
-    if [ $benchname = "gcc_r" ] ; then
+    if [ $benchname = "gcc_s" ] ; then
         echo "detected gcc, running on core $core"
-        $UPROF $CORE_PIN $rundir/cpugcc_r_base.aocc-3-3.1.0-m64 $(flags $benchname) > $outloc/$instance.out 2>> $outloc/$instance.err &
-    elif [ $benchname = "xalancbmk_r" ] ; then
+        $UPROF $CORE_PIN $rundir/cpugcc_s_base.aocc-3-3.1.0-m64 $(flags $benchname) > $outloc/$instance.out 2>> $outloc/$instance.err &
+    elif [ $benchname = "xalancbmk_s" ] ; then
         echo "detected xalancbmk, running on core $core"
-        $UPROF $CORE_PIN $rundir/cpuxalan_r_base.aocc-3-3.1.0-m64 $(flags $benchname) > $outloc/$instance.out 2>> $outloc/$instance.err &
-    elif [ $benchname = "cactuBSSN_r" ] ; then
+        $UPROF $CORE_PIN $rundir/cpuxalan_s_base.aocc-3-3.1.0-m64 $(flags $benchname) > /dev/null 2>> $outloc/$instance.err &
+    elif [ $benchname = "cactuBSSN_s" ] ; then
         echo "detected cactuBSSN, running on core $core"
-        $UPROF $CORE_PIN $rundir/cactusBSSN_r_base.aocc-3-3.1.0-m64 $(flags $benchname) > $outloc/$instance.out 2>> $outloc/$instance.err &
+        $UPROF $CORE_PIN $rundir/cactusBSSN_s_base.aocc-3-3.1.0-m64 $(flags $benchname) > $outloc/$instance.out 2>> $outloc/$instance.err &
     #benchmarks that use bash tokens in the parameters
-    elif [ $benchname = "bwaves_r" ] ; then
+    elif [ $benchname = "bwaves_s" ] ; then
         echo "detected bwaves, running on core $core"
         $UPROF $CORE_PIN $rundir/$benchname\_base.aocc-3-3.1.0-m64 bwaves_1 < bwaves_1.in > $outloc/$instance.out 2>> $outloc/$instance.err &
-    elif [ $benchname = "roms_r" ] ; then
+    elif [ $benchname = "roms_s" ] ; then
         echo "detected roms, running on core $core"
         $UPROF $CORE_PIN $rundir/$benchname\_base.aocc-3-3.1.0-m64 < ocean_benchmark2.in.x > $outloc/$instance.out 2>> $outloc/$instance.err &
     #everything else
@@ -133,11 +130,28 @@ function run_benchmark {
     fi
 }
 
-for i in $(ls -1 | grep "_r"); do
+for i in $(ls -1 | grep "_s"); do
     export RUN_DIR=$(pwd)/$i/run/run_base_refrate_aocc-3-3.1.0-m64.0000
+
+    cd /home/student/pal0009/CPE-631-Term-Project/run_scripts
+    ./debug_suite.sh $i &
+    sleep 30
+    export bname=$(cut -d '.' -f2- <<< $i)
+    if [ $bname = "gcc_s" ] ; then
+        export bname="cpugcc_s"
+    elif [ $bname = "xalancbmk_s" ] ; then
+        export bname="cpuxalan_s"
+    elif [ $bname = "cactuBSSN_s" ] ; then
+        export bname="cactusBSSN_s"
+    fi
+    pkill $bname\*
+    pkill AMDuProfPcm
+
+    sleep 5
+
     cd $RUN_DIR
-    for j in 1 ; do
-        export RESULT_LOC=/home/student/pal0009/CPE-631-Term-Project/scale_results/$i\_$j
+    for j in 48 ; do
+        export RESULT_LOC=/home/student/pal0009/CPE-631-Term-Project/uprof_results/$i
         rm -rf $RESULT_LOC
         mkdir $RESULT_LOC
         
@@ -147,7 +161,7 @@ for i in $(ls -1 | grep "_r"); do
         done
         wait
 
-        rm $RESULT_LOC/*.out
+        rm -f $RESULT_LOC/*.out
     done
     cd ../../..
 done
